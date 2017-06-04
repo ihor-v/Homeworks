ef3e067cc38c0cfcda2501bf98a69815fc9ad6a0
Ihor Shcherbatyy <igorstscherbatyj@gmail.com>
2017-06-04 23:44:28 +0300

    new adds

--- Homework04/src/carPackage/Car.java
+++ Homework04/src/carPackage/Car.java
@@ -6,12 +6,28 @@ public class Car {
 	int rozhid;
 	int objem;
 	int probih;
-	Car() {
+	boolean isEmpty;
+
+	Car(String model) {
+		color = "Green";
+		this.model = model;
+		rozhid = 6;
+		objem = 25;
+		isEmpty = false;
+		probih = 130256;
 
 	}
+
 	void drive(int km) {
-		if (objem / rozhid > km) {
-			System.out.println("Test one");
+		if (isEmpty) {
+			System.out.println("Empty Fuel Tank");
+		} else if (objem / rozhid * 100 > km) {
+			this.probih = this.probih + km;
+			System.out.println(model + " has ride " + km + " and total is " + this.probih);
+		} else {
+			this.probih = this.probih + (objem / probih * 100);
+			System.out.println(model + " rided " + (objem / probih * 100) + " and total km is " + this.probih);
+			isEmpty = true;
 		}
 	}
 
--- Homework04/src/carPackage/Main.java
+++ Homework04/src/carPackage/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Test 2");
+		Car volvo = new Car("Volvo");
+		volvo.drive(726);
 	}
 
 }
