dffa5341d2e5db0fb7af9bf11076cc014439ca56
Ihor Shcherbatyy <igorstscherbatyj@gmail.com>
2017-06-04 22:22:29 +0300

    Initial submit

---
+++ Homework04/src/carPackage/Car.java
@@ -0,0 +1,25 @@
+package carPackage;
+
+public class Car {
+	String color;
+	String model;
+	int rozhid;
+	int objem;
+	int probih;
+	Car() {
+
+	}
+	void drive(int km) {
+		if (objem / rozhid > km) {
+			System.out.println();
+		}
+	}
+
+	void reVuel(int fuelVolume) {
+
+	}
+
+	void distance() {
+
+	}
+}
